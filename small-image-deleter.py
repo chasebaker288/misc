f0ac82cb7ce5cdfbd290fd716e2bf17609f44cd3
Chase <chasebaker288@gmail.com>
2018-05-10 13:50:48 -0500

    Update small-image-deleter.py

--- small-image-deleter.py
+++ small-image-deleter.py
@@ -4,24 +4,27 @@ from PIL import Image
 import os
 
 DIRECTORY = "C:/Users/My Dell/Google Drive/"
+CRITERIA_WIDTH = 200
+CRITERIA_HEIGHT = 200
+
 
 trashlist = []
 
 for file_name in os.listdir(DIRECTORY):
-	try:
-		img = Image.open(DIRECTORY + file_name)
-		width, height = img.size
-		if width < 200:
-			trashlist.append(file_name)
-		elif height < 200:
-			trashlist.append(file_name)
-	except:
-		pass
+    try:
+        img = Image.open(DIRECTORY + file_name)
+        width, height = img.size
+        if width < CRITERIA_WIDTH:
+            trashlist.append(file_name)
+        elif height < CRITERIA_HEIGHT:
+            trashlist.append(file_name)
+    except:
+        pass
 
 for i in trashlist:
-	for j in [".jpeg", ".jpg", ".png", ".gif"]:
-		if i.endswith(j):
-			os.remove(DIRECTORY + i)
-			print("Removed ", i)
+    for j in [".jpeg", ".jpg", ".png", ".gif"]:
+        if i.endswith(j):
+            os.remove(DIRECTORY + i)
+            print("Removed ", i)
 
 print(str(len(trashlist)) + " items deleted.")
